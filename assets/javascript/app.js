e1f5ee9fe0f2c8f05db8fdd5217b27880e114827
Chaucee <hi@chaucee.com>
2017-12-10 10:55:44 -0700

    sunday 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,22 @@
-// Game loads with Start Button
-// Timer countdown starts 
-// User clicks Start and five questions load with for answer options
+// TIMER
+var startTimer = new Date;
+
+setInterval(function() {
+    $('#timer').text((new Date - startTimer) / 1000 + " Seconds");
+}, 1000);
+
+// somehow need to tell the timer to wait to load until the start button is clicked.
 // Once the timer runs out screen display turns to "All done! Correct answers: Incorrect Answers: Unaswered:"
+
+
+// START BUTTON
+$('.startButton').on('click', function () {
+    $('.startButton').remove().on('click', startTimer());
+    displayQuestion();
+});
+
+// User clicks Start and five questions load with for answer options...questions shoudld be hid before that.
+
 
 // DEFINE VARAIBLES
 
@@ -10,22 +25,28 @@ var correct = 0;
 var inccorect = 0;
 var unasnwered = 0;
 
-
-var q1 = "Which actor doesn't like coffee in real life?";
-var q2 = "Who speaks the first line in the series?";
+var q1 = "Which actor actually doesn't like coffee?";
+var q2 = "Who speaks the first line in the series?"
 var q3 = "Which major character was originally scripted as a woman?";
 var q4 = "Which musician sings the Gilmore Girls theme song?";
 var q5 = "What is the population of Stars Hollow?";
 
-var a1 = ["Lauren Graham", "Alexis Bledel", "182 million", "205 million"];
-var a2 = ["Luke", "Lorelai", "Sooki", "Rory"];
-var a3 = ["Luke", "Lorelai", "Sooki", "Rory"];
-var a4 = ["Carole King", "Lisa Loeb", "Jewel", "Beyonce"];
-var a5 = ["9,973", "10,015", "8,567", "9,374"];
+var a1 = ["Lauren Graham ", "Alexis Bledel ", "Scott Patterson ", "Kelly Bishop "];
+var a2 = ["Luke ", "Lorelai ", "Sooki ", "Rory "]; 
+var a3 = ["Luke ", "Lorelai ", "Sooki ", "Rory "];
+var a4 = ["Carole King ", "Lisa Loeb ", "Jewel ", "Beyonce "];
+var a5 = ["9,973 ", "10,015 ", "8,567 ", "9,374 "];
+
 
 
 // DEFINE FUNCTIONS
 
+// Function to compare user choice against the correct answers
+// If user choice is correct then icrease var correct by one
+// If user choice is incorrect then icrease var incorrect by one
+// if user selects no choice for a question increase var unanswered by one
+// Submit function
+    
 
 
 // CALL FUNCTIONS
@@ -36,10 +57,12 @@ $('.question3').append(q3);
 $('.question4').append(q4);
 $('.question5').append(q5);
 
-$(document).find(".quizContainer > .choiceList");
+$('.choiceList1').append(a1);
+$('.choiceList2').append(a2);
+$('.choiceList3').append(a3);
+$('.choiceList4').append(a4);
+$('.choiceList5').append(a5);
 
-$('.choiseList1').append(a1);
-$('.choiseList2').append(a2);
-$('.choiseList3').append(a3);
-$('.choiseList4').append(a4);
-$('.choiseList5').append(a5);
+
+
+
