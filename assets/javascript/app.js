d8774a572a539555c3ca3fd54530b8a5e2eb2811
Chaucee <hi@chaucee.com>
2017-12-10 08:20:26 -0700

    game style

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// Game loads with Start Button
+// Timer countdown starts 
+// User clicks Start and five questions load with for answer options
+// Once the timer runs out screen display turns to "All done! Correct answers: Incorrect Answers: Unaswered:"
